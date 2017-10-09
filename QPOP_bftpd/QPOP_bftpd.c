2a5063f416a0f7978a99cbabefc1440e13e7c178
Christian Palmiero <christian.alons@gmail.com>
2017-10-09 15:34:14 -0400

    End of Wilander testbed

---
+++ QPOP_bftpd/QPOP_bftpd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+char* dummy_args[] = {"buffer_ovf_2v", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\xe0\x03\x00\x00", NULL};
+
+void secretFunction()
+{
+    printf("Congratulations!\n");
+    printf("You have entered in the secret function!\n");
+}
+
+void echo(char* s)
+{
+    char buffer[20];
+
+    strcpy(buffer, s);
+    //printf("You entered: %s\n", buffer);    
+}
+
+int main(int argc, char* argv[])
+{
+    volatile int a = 1;
+    int i;
+    argv = dummy_args;
+    argc = sizeof(dummy_args)/sizeof(dummy_args[0])-1;
+
+    for(i=0; i<strlen(argv[1]); i++){
+      asm volatile ("p.spsw x0, 0(%[argv]);"                
+                    :
+                    :[argv] "r" (argv[1]+i));
+    }
+
+    if(a)
+      echo(argv[1]);
+    else
+      secretFunction();
+
+    return 0;
+}
